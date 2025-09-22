6aba36199794fcde0ac23c24bcc887353e53fe60
Onkar Chougule <onkarchougule99@gmail.com>
2025-09-22 23:23:35 +0530

    pages folder add

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css'
-
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import {Route,Routes,Navigate} from "react-router-dom"
 function App() {
-  
+
+  let authUser = null;
+
   return (
     <>
-      <h1 className='text-emerald-700 text-8xl font-bold'>Onkar Chougule</h1>
+      <div className='flex items-center justify-center h-screen'>
+      <Routes>
+        <Route path="/" element={authUser ? <Home/>: <Navigate to={"/login"}/>}/>
+        <Route path="/login" element={ !authUser ? <Login/> :<Navigate to={"/"}/> }/>
+        <Route path="/signup" element={!authUser ? <SignUp/>: <Navigate to={"/"}/>} />
+      </Routes > 
+      </div>
     </>
   )
 }
